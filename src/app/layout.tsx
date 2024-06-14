8a24bc22d00e222dfcbaa4f8affc738652c25b85
Avishake Adhikary <avhishe.adhikary11@gmail.com>
2024-06-14 15:03:17 +0530

    Updated OpenGraph MetaData

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Avishake's Portfolio",
   description: "Avishake Adhikary - Machine Learning Engineer | Full Stack Developer | Explore the dynamic portfolio of Avishake Adhikary, a proficient Machine Learning Engineer and Full Stack Developer based in Kolkata, India. With a strong foundation in Java, JavaScript, Python, and more, Avishake specializes in crafting innovative solutions that blend cutting-edge technology with user-centric design. Discover his expertise in AI, web development, and cloud computing through hands-on projects and professional experiences. Get in touch to collaborate on transformative tech ventures.",
+  openGraph: {
+    images: [
+      {
+        url: "https://github.com/AvishakeAdhikary/avishakeadhikary.github.io/blob/main/public/images/gallery/AvishakeAmityMerc.png?raw=true",
+        width: 4032,
+        height: 3024,
+      },
+    ]
+  }
 };
 
 export default function RootLayout({
