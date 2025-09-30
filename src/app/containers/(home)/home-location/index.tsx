c25aad5a95c4939c6787c2f03cd5eb8c89a0c9b4
Avishake Adhikary <avhishe.adhikary11@gmail.com>
2025-10-01 00:53:44 +0530

    Upgraded to tailwind 4 and fixed dark mode

--- src/app/containers/(home)/home-location/index.tsx
+++ src/app/containers/(home)/home-location/index.tsx
@@ -74,7 +74,7 @@ export default function LocationSection() {
             But, I handle projects all over the world...
           </p>
         </motion.div>
-        <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
+        <div className="absolute w-full bottom-0 inset-x-0 h-40 pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
         <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
           <World data={kolkataArcs} globeConfig={globeConfig} />
         </div>
